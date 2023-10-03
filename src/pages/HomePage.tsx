ad7919be1ef916e9339eb0c11f4c31a84f3267c5
David Yanez <davidyanez-27@hotmail.com>
2023-10-03 16:54:45 -0600

    feat: assets for the sidebar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import arrow from "../assets/previous.png";
-import logo from "../assets/legal-document.png";
+import logo from "../assets/logo.png";
 
 function Home() {
   const [open, setOpen] = useState(true);
   const Menus = [
-    { title: "Dashboard", src: "Chart_fill" },
-    { title: "Inbox", src: "Chat" },
+    { title: "Templates", src: "Chart_fill" },
+    { title: "my templates", src: "Chat" },
     { title: "Accounts", src: "User", gap: true },
     { title: "Schedule ", src: "Calendar" },
     { title: "Search", src: "Search" },
@@ -19,12 +19,12 @@ function Home() {
     <div className="text-black">
       <div
         className={`${
-          open ? "w-72" : "w-20"
-        } p-5 pt-8 duration-300 h-screen bg-slate-600 relative`}
+          open ? "w-72" : "w-32"
+        } p-5 pt-8 duration-300 h-screen bg-slate-600 rounded-2xl relative`}
       >
         <img
           src={arrow}
-          className={`bg-slate-400 rounded-full absolute cursor-pointer right-3 top-9 w-7 border-2 ${
+          className={`bg-slate-400 rounded-full absolute cursor-pointer right-3 top-20 w-7 border-2 ${
             !open && "rotate-180"
           }`}
           onClick={() => setOpen(!open)}
@@ -33,15 +33,15 @@ function Home() {
         <div className="flex gap-x-4 items-center">
           <img
             src={logo}
-            className={` bg-indigo-100 w-7 h-7 cursor-pointer duration-500`}
+            className={` bg-transparent w-12 h-12 cursor-pointer duration-500`}
           ></img>
-          <h1
+          {/* <h1
             className={`text-white origin-left font-medium text-xl duration-300 ${
               !open && "scale-0"
             }`}
           >
-            Designer
-          </h1>
+            APLINIO CAMPOS
+          </h1> */}
         </div>
         <ul className="pt-6">
           {Menus.map((menu, index) => (
@@ -49,13 +49,14 @@ function Home() {
               key={index}
               className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md ${menu.gap ? "mt-9": "mt-2"}`}
             >   
+              <img src={`./src/assets/assets/${menu.src}.png`}/>
               <span className={`${!open && 'hidden'} origin-left duration-200`}>{menu.title}</span>
             </li>
           ))}
         </ul>
       </div>
       <div className="p-7 text-2xl font-semibold">
-        <h1>Home Page</h1>
+    
       </div>
     </div>
   );
